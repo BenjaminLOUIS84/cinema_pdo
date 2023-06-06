5137a9fbea80f3124a2c3fded41aee75381f8339
Benjamin Louis <louisbenetval@orange.fr>
2023-06-06 08:50:44 +0200

    MAJ

--- bdd/DAO.php
+++ bdd/DAO.php
@@ -1,7 +1,10 @@
-<!-- définition
+<!-- DEFINITIONS
 
-DAO Data Accès Object
-PDO  -->
+DAO Data Accès Object Patron de conception utilisé dans les architectures logicielles objet, regroupe les accès aux données persistantes dans des classes à part plutôt que de les disperser.
+PDO PHP Data Object Pour dialoguer avec MySQL depuis PHP constitue une couche d'abstraction qui intervient entre l'application PHP et un système de gestion de base de données (SGDB) tel que MySQL, PostgreSQL ou MariaDB par exemple. 
+PDO::query — Prépare et Exécute une requête SQL sans marque substitutive
+PDO::prepare — Prépare une requête à l'exécution et retourne un objet
+-->
 
 <?php
 
@@ -19,7 +22,7 @@ class DAO{
     public function executerRequete($sql, $params = NULL){
         if ($params == NULL){
             $resultat = $this->bdd->query($sql);
-            //query à définir
+            
         }else{
             $resultat = $this->bdd->prepare($sql);
             $resultat->execute($params);
