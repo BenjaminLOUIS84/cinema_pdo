d23b97b6595f1363aff0a6e0102af991435b086a
Benjamin Louis <louisbenetval@orange.fr>
2023-06-08 00:46:59 +0200

    MAJ

--- views/role/detailRole.php
+++ views/role/detailRole.php
@@ -13,11 +13,23 @@
 
         echo $role["perso"]."<br>";
         echo "<br>";
-        echo " Ce rôle est interprété par ";
-        echo $role["prenom"]." ";                                                   
-        echo $role["nom"]." "."dans<br>";
-        
-        //echo "dans le film: ".$role["titre"]."<br>";
+        echo " Ce rôle est interprété par <br>";
+
+    
+        //echo $role["prenom"]." ";                                                   
+        //echo $role["nom"]."<br>";
+    ?>
+
+    <a class="detail" href="index.php?action=detailActor&idActor=<?=$role['id_acteur']?>">
+        <h2><?=$role["prenom"]?></h2>    
+        <h2><?=$role["nom"]?></h2>    
+    </a>
+
+    <?php
+
+        echo "dans<br>";
+
+        //echo $role["titre"]."<br>";
     ?>
 
     <a class="detail" href="index.php?action=detailMovie&idFilm=<?=$role['id_film']?>">
