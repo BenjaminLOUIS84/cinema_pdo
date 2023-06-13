5e052c0bcf40480fb811da23827623ae2418884d
Benjamin Louis <louisbenetval@orange.fr>
2023-06-14 01:12:39 +0200

    MAJ

--- controllers/MovieController.php
+++ controllers/MovieController.php
@@ -4,15 +4,15 @@
 
     class MovieController {
 
-        public function findAllMovies(){                                                                         //Fonction pour afficher la liste des films
+        public function findAllMovies(){                                                                         // Fonction pour afficher la liste des films
 
-            $dao = new DAO();
+            $dao = new DAO();                                                                                    // On instancie le DAO pour se connecter à la BDD
             $sql = "SELECT f.id_film, f.titre  FROM film f";
             $films = $dao->executerRequete($sql);
             require "views/movie/listMovies.php"; 
         }
 
-        public function detailMovie($idFilm){                                                                    //Fonction pour accéder au détails des films
+        public function detailMovie($idFilm){                                                                    // Fonction pour accéder au détails des films
 
             $dao = new DAO();
 
@@ -22,9 +22,9 @@
             WHERE f.id_realisateur = r.id_realisateur
             AND p.id_personne = r.id_personne
             
-            AND f.id_film = :id" ;                                                                              //Pour empécher le piratage on met la clé dans un opérateur de solution de portée 
+            AND f.id_film = :id" ;                                                                              // Pour empécher le piratage on met la clé dans un opérateur de solution de portée 
 
-            $films = $dao->executerRequete($sql, [":id" => $idFilm]);                                           //Faire une requête préparée
+            $films = $dao->executerRequete($sql, [":id" => $idFilm]);                                           // Faire une requête préparée
             
             ////////////////////////////////////////////////////////////Requête SQL2 DISTRIBUTION
         
@@ -55,72 +55,61 @@
 
         ///////////////////////////////////////////////////////////FORMULAIRE
 
-        public function openFormulaireMovie(){                                                                  //Fonction pour accéder au formulaire
+        public function openFormulaireMovie(){                                                                  // Fonction pour accéder au formulaire
             
-            $dao = new DAO();                                                                                   //Lier les réalisateurs aux films grâce à une requête SQL
+            $dao = new DAO();                                                                                   // Lier les réalisateurs et les genres aux films grâce à une requête SQL
             
-            // $sql = "SELECT p.prenom, p.nom, r.id_realisateur
-            // FROM personne p
-            // INNER JOIN realisateur r
-            // ON r.id_personne = p.id_personne
-            // AND r.id_realisateur = :id" ; 
-            
-           // $realisateurs = $dao->executerRequete($sql, [":id" => $idDirector]);
+            $sql = "SELECT p.prenom, p.nom, r.id_realisateur
+            FROM personne p
+            INNER JOIN realisateur r
+            ON r.id_personne = p.id_personne" ; 
+            $realisateurs = $dao->executerRequete($sql);
+
+            $sql2 = "SELECT g.type, g.id_genre
+            FROM genre g ";            
+            $genres = $dao->executerRequete($sql2);
 
             require "views/movie/formulaireMovie.php"; 
         }
 
-        public function addMovie($array){                                                                       //Fonction pour ajouter un nouveau film dans la base SQL via le formulaire
+        public function addMovie($array){                                                                       // Fonction pour ajouter un nouveau film dans la base SQL via le formulaire
             
-            $dao = new DAO();                                                                                   //Requête SQL INSERT INTO table(...) VALUES (...) pour ajouter un film, celui ci comprendra un titre, une année de sortie,...(Ajouter les inputs en conséquence dans formulaireMovie)
-
-            $sql1 = "INSERT INTO film(titre, annee_sortie, duree, synopsis, note)                                                                              
-            VALUES (:titre, :annee_sortie, :duree, :synopsis, :note)";                                          //Les names des inputs doivent correspondre respectivement aux variables $titre, $annee_sortie,...
-            
-            $titre = filter_input(INPUT_POST, "titre", FILTER_SANITIZE_FULL_SPECIAL_CHARS);                     //Mettre les filtres aux inputs pour éviter les injections SQL ou XSS
+            $titre = filter_input(INPUT_POST, "titre", FILTER_SANITIZE_FULL_SPECIAL_CHARS);                     // Mettre les filtres aux inputs pour éviter les injections SQL ou XSS
             $annee_sortie = filter_input(INPUT_POST, "annee_sortie", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
             $duree = filter_input(INPUT_POST, "duree", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
             $synopsis = filter_input(INPUT_POST, "synopsis", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
             $note = filter_input(INPUT_POST, "note", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
-            
-            $ajout = $dao->executerRequete($sql1, ["titre" => $titre,"annee_sortie" => $annee_sortie,
-            "duree" => $duree, "synopsis" => $synopsis, "note" => $note]);                                      //Pour éxecuter les requêtes SQL et renvoyer les valeurs respectives  
+            $id_genres = filter_var_array($array['id_genre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);              // FILTER VAR ARRAY POUR LA SELECTION MULTIPLE DES GENRES id_genre deviendra un array
+            $id_realisateur = filter_input(INPUT_POST, "id_realisateur", FILTER_VALIDATE_INT);                  // Récupération de l'id_realisateur pour la jonction
 
-            $dernierID = $dao->getBDD()->lastInsertID();
-
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //Ajouter la requête SQL pour ajouter un genre au film
-
-            $sql2 = "INSERT INTO genre(id_genre, id_film)                                                                              
-            VALUES (:idGenre, :idFilm)";                                                                         //Les names des inputs doivent correspondre respectivement aux variables $titre, $annee_sortie,...
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //Ajouter les requêtes SQL pour ajouter les films et les genres aux films
             
-            $genre_film = filter_input_array(INPUT_POST, "genref", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
-            
-            foreach ($genre_film as $genreActuel){
-            
-                $ajoutGenre = $dao->executerRequete($sql2, ["idGenre" => $genreActuel,"idFilm" => $dernierID]);
+            $dao = new DAO();                                                                                   // Requête SQL INSERT INTO table(...) VALUES (...) pour ajouter un film, celui ci comprendra un titre, une année de sortie,...(Ajouter les inputs en conséquence dans formulaireMovie)
 
+            $sql1 = "INSERT INTO film(titre, annee_sortie, duree, synopsis, note, id_realisateur)                                                                              
+            VALUES (:titre, :annee_sortie, :duree, :synopsis, :note, :id_realisateur)";                         // Les names des inputs doivent correspondre respectivement aux variables $titre, $annee_sortie,...
+            
+
+            $sql2 = "INSERT INTO classer(id_genre, id_film)                                                                              
+            VALUES (:id_genre, :id_film)";                                                                      
+            
+            $ajouterFilm = $dao->executerRequete($sql1, ["titre" => $titre,"annee_sortie" => $annee_sortie,
+            "duree" => $duree, "synopsis" => $synopsis, "note" => $note, "id_realisateur" => $id_realisateur]); // Pour éxecuter les requêtes SQL et renvoyer les valeurs respectives  
+
+            $id_new_film = $dao->getBDD()->lastInsertId();                                                      // Récupèrer l'ID auto incrémenté qui s'est créé lors de l'ajout du film
+            
+            foreach ($id_genres as $id_genre){
+            
+                $ajouterGenre = $dao->executerRequete($sql2, ["id_genre" => $id_genre,"id_film" => $id_new_film]);
             }
-
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //Ajouter la requête SQL pour ajouter un réalisateur au film
-
-            // $sql2 = "INSERT INTO realisateur(prenom, nom)                                                                              
-            // VALUES (:prenom, :nom)";                                                                         //Les names des inputs doivent correspondre respectivement aux variables $titre, $annee_sortie,...
-            
-            // $prenom = filter_input(INPUT_POST, "prenom", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
-            // $nom = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
-
-            // $ajoutRealisateur = $dao->executerRequete($sql2, ["prenom" => $prenom,"nom" => $nom]);
-
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+           
+            $this->detailMovie($id_new_film);
 
             //Pour afficher un message Flash à chaque ajout inscrire cette variable dans chaque partie
 
             $_SESSION['flash_message'] = $titre." "."a été ajouté avec succès !";                               
-            $this->findAllMovies();                                                                             //Etre redirigé sur la même page  
+            $this->findAllMovies();                                                                             // Etre redirigé sur la même page  
         }
     }
 ?>
--- views/movie/formulaireMovie.php
+++ views/movie/formulaireMovie.php
@@ -12,66 +12,80 @@
 
 <div class="formulaire">
 
-    <h3>FILMS</h3>
+    <form action = "index.php?action=addMovie" method = "post">                             <!-- Le formulaire doit déclencher l'action de la fonction addMovie() -->
+            
+        <div class="formAdd">
 
-    <form action = "index.php?action=addMovie" method = "post">                         <!-- Le formulaire doit déclencher l'action de la fonction addMovie() -->
-
-        <p>
-            <label>
-                Titres <br>                                                              <!-- input (champs de saisi de texte) -->
-                <input class="name" type = "text" name = "titre" required>              <!-- Le name de cet input doit correspondre à la variable $titre -->
-            </label>
-            <label>
-                Années <br>                                                    <!-- input (champs de saisi de numérique ) -->
-                <input class="name" type = "int" name = "annee_sortie" required>        <!-- Le name de l'input doit correspondre à la variable $annee_sortie -->
-            </label>
-        </p> 
-        <p>
-            <label>
-                Durées <br>                                                          
-                <input class="name" type = "int" name = "duree" required>          
-            </label>
-            <label>
-                Notes <br>                                                          
-                <input class="name" type = "int" name = "note" required>          
-            </label>
-        </p>
-        <p>
-            <label>
-                Synopsis <br>                                                          
-                <input class="nameSynopsis" type = "text" name = "synopsis" required>          
-            </label>
-        </p> 
+            <h3>FILMS</h3>
+            <p>
+                <label>
+                    Titres <br>                                                                 <!-- input (champs de saisi de texte) -->
+                    <input class="name" type = "text" name = "titre" required>                  <!-- Le name de cet input doit correspondre à la variable $titre -->
+                </label>
+                <label>
+                    Années <br>                                                                 <!-- input (champs de saisi de numérique ) -->
+                    <input class="name" type = "int" name = "annee_sortie" required>            <!-- Le name de l'input doit correspondre à la variable $annee_sortie -->
+                </label>
+            </p> 
+            <p>
+                <label>
+                    Durées <br>                                                          
+                    <input class="name" type = "int" name = "duree" required>          
+                </label>
+                <label>
+                    Notes <br>                                                          
+                    <input class="name" type = "int" name = "note" required>          
+                </label>
+            </p>
+            <p>
+                <label>
+                    Synopsis <br>                                                          
+                    <input class="nameSynopsis" type = "text" name = "synopsis" required>          
+                </label>
+            </p> 
         
+        </div>
 
         <div class="formAdd">
 
             <h3>GENRES</h3>
 
-            <p>
-                <label>                                                                     <!-- Lier les réalisateurs aux films Ajouter l'input pour saisir le réalisateur-->
-                    Références <br>                                                          
-                    <input class="nameGenre" type = "int" name = "genref" required>          
-                </label>
-            </p>
+            <!-- Lier les genres aux Films en créant une liste déroulante --> 
+
+            <select class="nameGenre" name = "id_genre[]" required> 
+                <option selected>Genre</option> 
+                
+                <?php
+
+                    while ($genre = $genres->fetch()){                                              // Utilisaion d'un fetch() pour que les Genres soient dans la liste
+
+                        echo "<option value =".$genre['id_genre'].">".$genre['type']."</option>";   // La value permet de récupérer l'id_genre
+                    }
+                ?>
+            </select>
+
         </div>
 
-        <!-- <div class="formAdd"> -->
+        <div class="formAdd">
 
-            <!-- <h3>REALISATEURS</h3> -->
+            <h3>REALISATEURS</h3>
 
-            <p>
-                <label>                                                                     <!-- Lier les réalisateurs aux films Ajouter l'input pour saisir le réalisateur-->
-                    <!-- Noms <br>                                                           -->
-                    <!-- <input class="name" type = "text" name = "nom" required>           -->
-                </label>
-                <label>                                                                     
-                    <!-- Prénoms <br>                                                           -->
-                    <!-- <input class="name" type = "text" name = "prenom" required>           -->
-                </label>
-            </p>
-        <!-- </div> -->
+            <!-- Lier les réalisateurs aux films en créant aussi une liste déroulante-->
 
+            <select class="nameRealisateur" type = "text" name = "id_realisateur" required>          
+                <option selected>Realisateur</option>
+            
+                <?php
+
+                    while ($realisateur = $realisateurs->fetch()){
+
+                        echo "<option value = ".$realisateur['id_realisateur'].">".$realisateur['prenom']." ".$realisateur['nom']."</option>";
+                    }
+                ?>
+            </select>
+
+        </div>
+ 
         <p>                                                                             <!-- Bouton pour envoyer la demande au MovieController -->
             <input class="add" type = "submit" name = "addMovie" value = "AJOUTER">     <!-- Le name de l'input doit correspondre à la fonction  addMovie() -->
         </p>
@@ -89,4 +103,3 @@
     require "views/template.php";
 
 ?>
-
