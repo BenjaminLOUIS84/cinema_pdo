424b39b8d729a155d9784c409c5ced6fbefb28d9
Benjamin Louis <louisbenetval@orange.fr>
2023-06-18 23:08:19 +0200

    MAJ

--- views/role/formulaireRole.php
+++ views/role/formulaireRole.php
@@ -9,55 +9,59 @@
 
 <div class="formulaire">
 
-    <h3>ROLES</h3>
+    <div class="formAdd">
+            
+        <h3>ROLES</h3>
 
-    <form action = "index.php?action=addRole" method = "post">                         <!-- Pour ajouter un nouveau Rôle -->
+        <form action = "index.php?action=addRole" method = "post">                         <!-- Pour ajouter un nouveau Rôle -->
 
-        <p>
-            <label>
-                Prénom <br>
-                <input class="nameGenre" type = "text" name = "firstname" required>          <!-- Champs de texte de saisie -->           
-            </label>
-            <label>
-                Nom <br>
-                <input class="nameGenre" type = "text" name = "name" required>          <!-- Champs de texte de saisie -->           
-            </label>
-        </p> 
-        <p>
-            <label>
-                Pseudo <br>
-                <input class="nameGenre" type = "text" name = "pseudo" required>          <!-- Champs de texte de saisie -->           
-            </label>
-        </p> 
+            <p>
+                <label>
+                    Prénom <br>
+                    <input class="nameGenre" type = "text" name = "firstname" required>          <!-- Champs de texte de saisie -->           
+                </label>
+                <label>
+                    Nom <br>
+                    <input class="nameGenre" type = "text" name = "name" required>          <!-- Champs de texte de saisie -->           
+                </label>
+            </p> 
+            <p>
+                <label>
+                    Pseudo <br>
+                    <input class="nameGenre" type = "text" name = "pseudo" required>          <!-- Champs de texte de saisie -->           
+                </label>
+            </p> 
 
-        <p>
-            <input class="add" type = "submit" name = "addRole" value = "AJOUTER">     <!-- Bouton pour envoyer la demande -->
-        </p>
+            <p>
+                <input class="add" type = "submit" name = "addRole" value = "AJOUTER">     <!-- Bouton pour envoyer la demande -->
+            </p>
 
-    </form><br>
+        </form><br>
 
-    <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+        <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 
-    <form action = "index.php?action=delRole" method = "post">                         
+        <form action = "index.php?action=delRole" method = "post">                         
 
-        <p>
-        <select class="nameGenre" name = "role_acteur" required>                                               
-            <option selected>Rôles</option> 
-                
-            <?php
-                while ($role = $roles->fetch()){                                              // Utilisaion d'un fetch() pour que les Rôles soient dans la liste
+            <p>
+            <select class="nameGenre" name = "role_acteur" required>                                               
+                <option selected>Rôles</option> 
+                    
+                <?php
+                    while ($role = $roles->fetch()){                                              // Utilisaion d'un fetch() pour que les Rôles soient dans la liste
 
-                    echo "<option value =".$role['role_acteur'].">".$role['firstname']." ".$role['name']." ".$role['pseudo']."</option>";   // La value permet de récupérer l'id role_acteur
-                }
-            ?>
-        </select>
-        </p> 
+                        echo "<option value =".$role['role_acteur'].">".$role['firstname']." ".$role['name']." ".$role['pseudo']."</option>";   // La value permet de récupérer l'id role_acteur
+                    }
+                ?>
+            </select>
+            </p> 
 
-        <p>
-            <input class="add" type = "submit" name = "delRole" value = "SUPPRIMER">           
-        </p>
+            <p>
+                <input class="add" type = "submit" name = "delRole" value = "SUPPRIMER">           
+            </p>
 
-    </form>
+        </form>
+
+    </div>
 
 </div>
 
