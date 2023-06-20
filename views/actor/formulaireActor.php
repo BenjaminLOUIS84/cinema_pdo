7b6cf1ad775536356e5bab639f6fc98046f9239c
Benjamin Louis <louisbenetval@orange.fr>
2023-06-20 10:39:20 +0200

    MAJ

--- views/actor/formulaireActor.php
+++ views/actor/formulaireActor.php
@@ -13,73 +13,57 @@
 
         <h3>ACTEURS</h3>
 
-        <form action = "index.php?action=addActor" method = "post">
+        <form action="index.php?action=addActor" method="post">
 
-            <p>
-                <label>
-                    Nom <br>
-                    <input class="nameR" type = "text" name = "nom" required>
-                </label>
-                <label>
-                    Prénom <br>
-                    <input class="nameR" type = "text" name = "prenom" required>
-                </label>
-            </p>
-            <p>
-                <label>
-                    Sexe <br>
-                    <input class="nameR" type = "text" name = "sexe" required>
-                </label>
-                <label>
-                    Date de naissance <br>
-                    <input class="nameR" type = "date" name = "date_naissance" required>
-                </label>
-            </p>
-
-            <p>
-                <input class="add" type = "submit" name = "addActor" value = "AJOUTER">  
-            </p>
-
+            <label for="nom">Nom</label>
+            <input id="nom" class="nameR" type="text" name="nom" required>
+            
+            <label for="prenom">Prénom</label>
+            <input id=prenom" class="nameR" type="text" name="prenom" required>
+            
+            <label for="sexe">Sexe</label>
+            <input id="sexe" class="nameR" type="text" name="sexe" required>
+            
+            <label for="date_naissance">Date de naissance</label>
+            <input id="date_naissance" class="nameR" type="date" name="date_naissance" required>
+            
+            <input class="add" type="submit" name="addActor" value="AJOUTER">  
+            
             <!-- Le bouton AJOUTER va envoyer la demande POST pour exécuter le cas (case) "addActor" à l'index.php
             l'index.php obtient la demande GET et va a ensuite exécuter le cas pour envoyer POST à PersonController,
             PersonneController va exécuter la fonction addActor() qui contient les requêtes SQL pour ajouter une nouvelle personne avec le statut d'acteur -->
 
-        </form><br>
+        </form>
 
-        <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+        <form action="index.php?action=delActor" method="post">                         
 
-        <form action = "index.php?action=delActor" method = "post">                         
+            
+            <select class="nameR" type="text" name="id_acteur" required>                                               
+                <option selected>Acteurs</option> 
+    
+                <?php                                                                       // Avec le type <select><option> les acteurs sont dans une liste déroulante
+                    while ($acteur = $acteurs->fetch()){                                    // Utilisaion d'un fetch() pour que les acteurs soient dans la liste
 
-            <p>
-                <select class="nameR" type = "text" name = "id_acteur" required>                                               
-                    <option selected>Acteurs</option> 
+                        echo "<option value=".$acteur['id_acteur'].">
+                        ".$acteur['prenom']." ".$acteur['nom']."</option>";                 // La value permet de récupérer l'id_acteur et d'afficher le nom et le prénom d'un acteur
+                    }           
+                ?>
+            </select>
+             
+            <input class="add" type="submit" name="delActor" value="SUPPRIMER">           
         
-                    <?php                                                                       // Avec le type <select><option> les acteurs sont dans une liste déroulante
-                        while ($acteur = $acteurs->fetch()){                                    // Utilisaion d'un fetch() pour que les acteurs soient dans la liste
-
-                            echo "<option value =".$acteur['id_acteur'].">
-                            ".$acteur['prenom']." ".$acteur['nom']."</option>";                 // La value permet de récupérer l'id_acteur et d'afficher le nom et le prénom d'un acteur
-                        }           
-                    ?>
-                </select>
-            </p> 
-
-            <p>
-                <input class="add" type = "submit" name = "delActor" value = "SUPPRIMER">           
-            </p>
-
         </form>
 
     </div>
 
 </div>
 
-<a class ="detail" href="index.php">Retour</a>
+<a class="detail" href="index.php">Retour</a>
 
 <?php
 
     $title = "Formulaire Acteur";
-    $content = ob_get_clean();  // Récupérer et afficher puis nettoyer la mémoire tampon
+    $content = ob_get_clean();                                                                  // Récupérer et afficher puis nettoyer la mémoire tampon
     require "views/template.php";
 
 ?>
--- views/director/formulaireDirector.php
+++ views/director/formulaireDirector.php
@@ -13,70 +13,52 @@
 
         <h3>REALISATEURS</h3>
 
-        <form action = "index.php?action=addDirector" method = "post">
+        <form action="index.php?action=addDirector" method="post">
 
-            <p>
-                <label>
-                    Nom <br>
-                    <input class="nameR" type = "text" name = "nom" required>
-                </label>
-                <label>
-                    Prénom <br>
-                    <input class="nameR" type = "text" name = "prenom" required>
-                </label>
-            </p>
-            <p>
-                <label>
-                    Sexe <br>
-                    <input class="nameR" type = "text" name = "sexe" required>
-                </label>
-                <label>
-                    Date de naissance <br>
-                    <input class="nameR" type = "date" name = "date_naissance" required>
-                </label>
-            </p>
-
-            <p>
-                <input class="add" type = "submit" name = "addDirector" value = "AJOUTER">
-            </p>
-
-        </form><br>
-
-        <!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
-
-        <form action = "index.php?action=delDirector" method = "post">                         
-
-            <p>
-                <select class="nameR" type = "text" name = "id_realisateur" required>                                               
-                    <option selected>Réalisateurs</option> 
+            <label for="nom">Nom</label>
+            <input id="nom" class="nameR" type="text" name="nom" required>
+            
+            <label for="prenom">Prénom</label>
+            <input id="prenom" class="nameR" type="text" name="prenom" required>
+            
+            <label for="sexe">Sexe</label>
+            <input id="sexe" class="nameR" type="text" name="sexe" required>
+            
+            <label for="date_naissance">Date de naissance</label>
+            <input id="date_naissance" class="nameR" type="date" name="date_naissance" required>
+            
+            <input class="add" type="submit" name="addDirector" value="AJOUTER">
         
-                    <?php                                                                       // Avec le type <select><option> les réalisateurs sont dans une liste déroulante
-                        while ($realisateur = $realisateurs->fetch()){                          // Utilisaion d'un fetch() pour que les réalisateurs soient dans la liste
+        </form>
 
-                            echo "<option value =".$realisateur['id_realisateur'].">
-                            ".$realisateur['prenom']." ".$realisateur['nom']."</option>";       // La value permet de récupérer l'id_realisateur et d'afficher le nom et le prénom d'un réalisateur
-                        }           
-                    ?>
-                </select>
-            </p> 
+        <form action="index.php?action=delDirector" method="post">                         
 
-            <p>
-                <input class="add" type = "submit" name = "delDirector" value = "SUPPRIMER">           
-            </p>
+            <select class="nameR" type="text" name="id_realisateur" required>                                               
+                <option selected>Réalisateurs</option> 
+    
+                <?php                                                                       // Avec le type <select><option> les réalisateurs sont dans une liste déroulante
+                    while ($realisateur = $realisateurs->fetch()){                          // Utilisaion d'un fetch() pour que les réalisateurs soient dans la liste
 
+                        echo "<option value=".$realisateur['id_realisateur'].">
+                        ".$realisateur['prenom']." ".$realisateur['nom']."</option>";       // La value permet de récupérer l'id_realisateur et d'afficher le nom et le prénom d'un réalisateur
+                    }           
+                ?>
+
+            </select>
+             
+            <input class="add" type="submit" name="delDirector" value="SUPPRIMER">           
+            
         </form>
 
     </div>
 
 </div>
 
-<a class ="detail" href="index.php">Retour</a>
+<a class="detail" href="index.php">Retour</a>
 
 <?php
-
     $title = "Formulaire Réalisateur";
-    $content = ob_get_clean();  // Récupérer et afficher puis nettoyer la mémoire tampon
+    $content = ob_get_clean();                                                                  // Récupérer et afficher puis nettoyer la mémoire tampon
     require "views/template.php";
-
 ?>
 
